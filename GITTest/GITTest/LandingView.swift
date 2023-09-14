1bdc594dce1622317664db247e972f6fba18254d
Soumi Mandal <soumi.mandal@toyota.com>
2023-09-15 02:12:51 +0530

    main - 15th sep - 3rd

--- GITTest/GITTest/LandingView.swift
+++ GITTest/GITTest/LandingView.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 struct LandingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("This text is in main")
-        Text("This text is also in main")
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("This text is in main")
+            Text("This text is also in main")
+            Text("This again, main")
+        }
     }
 }
 
