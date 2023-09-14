30073db99df0a4ef66887b7606f952575534ac71
Soumi Mandal <soumi.mandal@toyota.com>
2023-09-14 12:07:27 +0530

    xcode project commit

---
+++ GITTest/GITTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GITTest
+//
+//  Created by karthikeyan.k47 on 14/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GITTest/GITTest/GITTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITTestApp.swift
+//  GITTest
+//
+//  Created by karthikeyan.k47 on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GITTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
