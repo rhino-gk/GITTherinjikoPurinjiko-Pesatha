3d3359cae25c028a713e3407e16ac534a84378ae
Soumi Mandal <soumi.mandal@toyota.com>
2023-09-14 12:16:16 +0530

    LandingView committed

---
+++ GITTest/GITTest/LandingView.swift
@@ -0,0 +1,20 @@
+//
+//  LandingView.swift
+//  GITTest
+//
+//  Created by karthikeyan.k47 on 14/09/23.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingView()
+    }
+}
