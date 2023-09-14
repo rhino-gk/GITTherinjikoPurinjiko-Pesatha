4009c3b71ca46b5d05d3c04e8cd659abf2a1c3bd
Soumi Mandal <soumi.mandal@toyota.com>
2023-09-14 14:24:33 +0530

    changes in main - 4

--- GITTest/GITTest/LandingView.swift
+++ GITTest/GITTest/LandingView.swift
@@ -11,6 +11,7 @@ struct LandingView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         Text("This text is in main")
+        Text("This text is also in main")
     }
 }
 
